d20da782b7152e073d0b38afb1fbf907f9fbff91
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-01-22 16:33:47 -0600

    WarmUp_4 Complete

--- WarmUp_4/ReverseeString.c
+++ WarmUp_4/ReverseeString.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(){
     char hello[] = "Hello";
-    printf(hello);
+    
+    printf("Original: %s \n", hello);
+    //found this using google
+    //int size = sizeof(hello) / sizeof(hello[0]); didn't work for the loop for whatever reason
+    int size = strlen(hello);
+    
+    for(int i = 0, j = size - 1; i < j; i++, j--){
+        char temp = hello[i];
+        hello[i] = hello[j];
+        hello[j] = temp;
+    }
+    printf("Reversed: %s \n", hello);
+    return 0;
 }
