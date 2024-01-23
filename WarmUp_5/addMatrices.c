dcbff63fe10e49f71e214568a0e7af2b6c30823b
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-01-23 12:28:15 -0600

    Guessing and confused

--- WarmUp_5/addMatrices.c
+++ WarmUp_5/addMatrices.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int** addMatrices(int** a, int** b){
+    int rows = sizeof(a)/sizeof(a[0]);
+    int cols = sizeof(b[0])/sizeof(b[0][0]);
+    int sum[rows][cols];
+
+for(int i = 0; i<rows; i++){
+    for(int j = 0; j<cols; j++){
+        sum[i][j]=a[i][j]+b[i][j];
+    }
+}
+return sum;
+}
