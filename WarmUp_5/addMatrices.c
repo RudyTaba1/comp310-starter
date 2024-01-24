8d389e1be2ed2014bb2bca93c617ac99a289fb4e
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-01-23 22:22:17 -0600

    making progress

--- WarmUp_5/addMatrices.c
+++ WarmUp_5/addMatrices.c
@@ -4,12 +4,23 @@
 int** addMatrices(int** a, int** b){
     int rows = sizeof(a)/sizeof(a[0]);
     int cols = sizeof(b[0])/sizeof(b[0][0]);
-    int sum[rows][cols];
+    //Doing research, couldn't figure out whether it is necessary to have malloc (memory allocation) to do this
+    //I think it is necessary, so I'm keeping it
+    int** sum = (int**)malloc(rows*sizeof(int*));
 
 for(int i = 0; i<rows; i++){
+    sum[i] = (int*)malloc(cols*sizeof(int));
     for(int j = 0; j<cols; j++){
         sum[i][j]=a[i][j]+b[i][j];
     }
 }
 return sum;
+}
+
+int** multiplyMatrcies(int** a, int** b){
+//storage method for product the same as sum    
+    int rows = sizeof(a)/sizeof(a[0]);
+    int cols = sizeof(b[0])/sizeof(b[0][0]);
+    int** product = (int**)malloc(rows*sizeof(int*));
+
 }
