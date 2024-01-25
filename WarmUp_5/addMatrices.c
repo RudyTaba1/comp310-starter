c6086580f0e46d011c10af177c02e6bdc5381662
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-01-25 15:20:17 -0600

    brother i don't like this, but coming along

--- WarmUp_5/addMatrices.c
+++ WarmUp_5/addMatrices.c
@@ -1,23 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
-    int matrix1[2][2] = {{1,2},{3,4}};
-    int matrix2[2][2] = {{5,6},{7,8}};
-
-    
-    int** sum = addMatrices(matrix1,matrix2);
-
-    int** product = multiplyMatrices(matrix1,matrix2);
-
-    printf("Matrix Addition: %d \n", printMatrix(sum));
-
-    printf("Matrix Multiplication: %d \n", printMatrix(product));
-
-    return 0;
-
-}
-
 int** addMatrices(int** a, int** b){
     int rows = sizeof(a)/sizeof(a[0]);
     int cols = sizeof(b[0])/sizeof(b[0][0]);
@@ -47,21 +30,40 @@ while(a[rows][cols] == b[rows][cols]){
         for(int j = 0; j<cols; j++){
             product[i][j]=a[i][j]*b[i][j];
         }
-    }
-    return product;
+    }    
+}
+return product;
 }
 
 int printMatrix(int**a){
-    rows = sizeof(a)/sizeof(a[0]);
-    cols = sizeof(a[0])/sizeof(a[0][0]);
+    int rows = sizeof(a)/sizeof(a[0]);
+    int cols = sizeof(a[0])/sizeof(a[0][0]);
     for(int i = 0; i<rows; i++){
         for(int j = 0; j<cols; j++){
             printf("%d ",a[i][j]);
         }
         printf("\n");
     }
+    return 0;
+}
+
+int main(){
+    int matrix1[2][2] = {{1,2},{3,4}};
+    int matrix2[2][2] = {{5,6},{7,8}};
+
+    
+    int** sum = addMatrices(matrix1,matrix2);
+
+    int** product = multiplyMatrcies(matrix1,matrix2);
+
+    printf("Matrix Addition: %d\n", printMatrix(sum));
+
+    printf("Matrix Multiplication: %d \n", printMatrix(product));
+
+    return 0;
+
 }
 
 
 
-}
+
