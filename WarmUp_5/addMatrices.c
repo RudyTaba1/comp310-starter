1795d204034d85d6846c803035cdcbc289060986
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-01-25 11:28:41 -0600

    Making slow progress. Getting fun conflicting error on line 21

--- WarmUp_5/addMatrices.c
+++ WarmUp_5/addMatrices.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int main(){
+    int matrix1[2][2] = {{1,2},{3,4}};
+    int matrix2[2][2] = {{5,6},{7,8}};
+
+    
+    int** sum = addMatrices(matrix1,matrix2);
+
+    int** product = multiplyMatrices(matrix1,matrix2);
+
+    printf("Matrix Addition: %d \n", printMatrix(sum));
+
+    printf("Matrix Multiplication: %d \n", printMatrix(product));
+
+    return 0;
+
+}
+
 int** addMatrices(int** a, int** b){
     int rows = sizeof(a)/sizeof(a[0]);
     int cols = sizeof(b[0])/sizeof(b[0][0]);
@@ -18,9 +35,33 @@ return sum;
 }
 
 int** multiplyMatrcies(int** a, int** b){
+    
 //storage method for product the same as sum    
     int rows = sizeof(a)/sizeof(a[0]);
     int cols = sizeof(b[0])/sizeof(b[0][0]);
     int** product = (int**)malloc(rows*sizeof(int*));
+
+while(a[rows][cols] == b[rows][cols]){
+    for(int i = 0; i<rows; i++){
+        product[i] = (int*)malloc(cols*sizeof(int));
+        for(int j = 0; j<cols; j++){
+            product[i][j]=a[i][j]*b[i][j];
+        }
+    }
+    return product;
+}
+
+int printMatrix(int**a){
+    rows = sizeof(a)/sizeof(a[0]);
+    cols = sizeof(a[0])/sizeof(a[0][0]);
+    for(int i = 0; i<rows; i++){
+        for(int j = 0; j<cols; j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 
 }
