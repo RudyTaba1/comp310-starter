640a22d72e410c3afa3a650e53c38966048d7a00
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-01-28 17:46:50 -0600

    WarmUp_9 Complete

--- WarmUp_9/FileOps.c
+++ WarmUp_9/FileOps.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main{
-char* filename = "example.txt";
+int main(){
+    char* filename = "example.txt";
 
-FILE* fw;
+    FILE* fw;
 
-fw = fopen("example.txt", )
+    fw = fopen(filename, "w");
+
+    if(fw != NULL){
+        fprintf(fw, "Hello World!");
+        fclose(fw);
+    }
+    else{
+        printf("Error opening file!");
+        exit(1);
+    }
+
+    FILE* fr;
+
+    fr = fopen(filename, "r");
+
+    if(fr != NULL){
+        char c;
+        while((c = fgetc(fr)) != EOF){
+            printf("%c", c);
+        }
+        
+        printf("\n");
+        fclose(fr);
+    }
+    else{
+        printf("Error opening file!");
+        exit(1);
+    }
+
+    return 0;
 }
